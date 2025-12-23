1ebc5c29b90335f06479ff97e8d21c5e79b4caef
Kevin Rosengren <rosenkev4@gmail.com>
2025-12-22 22:11:00 -0600

    Updated adder_test to print results

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,10 +1,15 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	actual := Adder(3, 10)
 	expected := 13
+
+	fmt.Printf("Actual: %d\nExpected: %d\n", actual, expected)
 
 	if actual != expected {
 		t.Errorf("Expected %d but got %d", expected, actual)
